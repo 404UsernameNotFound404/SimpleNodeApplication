6fba62eeb40e6a3b8b3f45f62908e46d739fee46
Henry Morris <hmorris@kinaxis.com>
2019-07-22 09:35:24 -0400

    updated git ignore

--- NodePraticeProgect/server.js
+++ NodePraticeProgect/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-const db = require('./src/config/db');
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
+const db = require("./src/config/db");
 
 const app = express();
 
@@ -9,12 +9,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
     if (err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+        console.log(
+            "Error occurred while connecting to MongoDB Atlas...\n",
+            err
+        );
     } else {
-        console.log('connected');
-        require('./src/routes')(app, database);
-        app.listen(3000, () => {
-            console.log('Listening on port 3000');
+        console.log("connected");
+        require("./src/routes")(app, database);
+        app.listen(8000, () => {
+            console.log("Listening on port 8000");
         });
     }
 });
